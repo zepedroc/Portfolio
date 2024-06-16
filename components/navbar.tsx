82acf81394e9a168a2745497043483de2f191f6a
zepedroc <jose.mota@emergn.com>
2024-06-16 21:44:17 +0100

    Navbar changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NavBar() {
   return (
-    <nav className="fixed left-0 top-0 z-50 flex w-full items-center justify-center bg-gray-950/50 px-6 py-4 backdrop-blur-md">
+    <nav className="fixed left-0 top-0 z-50 mx-auto mt-8 flex w-2/5 items-center justify-center rounded-xl bg-blue-500/50 py-4 backdrop-blur-md">
       <div className="flex items-center gap-6">
         <Link className="duration-400 transition-transform hover:scale-105 hover:underline" href="/">
           Home
@@ -11,7 +11,7 @@ export default function NavBar() {
           Work
         </Link>
         <Link
-          className="duration-800 rounded-md px-4 py-2 transition-transform hover:scale-105 hover:bg-gray-300 hover:text-black"
+          className="duration-400 transition-transform hover:scale-105 hover:underline"
           href="/resume.pdf"
           target="_blank"
         >
